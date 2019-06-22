ad9163a56a98000602d52ebbb547e203fd4c7c7f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-22 15:44:53 +0000

    Deploy muuvy/muuvy-frontend to github.com/muuvy/muuvy-frontend.git:gh-pages

---
+++ precache-manifest.0b5d49bf14af37fb7d613163b05b39bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161eb08ea2d60d918aaf88718251f4c1",
+    "url": "/muuvy-frontend/index.html"
+  },
+  {
+    "revision": "2903e6de7ec29f36ef84",
+    "url": "/muuvy-frontend/static/css/main.c13cf409.chunk.css"
+  },
+  {
+    "revision": "6a66335dc60769e43c9b",
+    "url": "/muuvy-frontend/static/js/2.42e45057.chunk.js"
+  },
+  {
+    "revision": "2903e6de7ec29f36ef84",
+    "url": "/muuvy-frontend/static/js/main.a539e1f7.chunk.js"
+  },
+  {
+    "revision": "429f066038a797a7a932",
+    "url": "/muuvy-frontend/static/js/runtime~main.e7a07fad.js"
+  }
+]);
